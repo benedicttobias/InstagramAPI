62c119fc104e8289408714d09dbdb7a996d30662
benedict.tobias <btobia9509@students.pcci.edu>
2017-06-20 22:31:25 -0500

    Call API successful

--- Instagram API Practice/Default.aspx
+++ Instagram API Practice/Default.aspx
@@ -1,41 +1,53 @@
-﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Instagram_API_Practice._Default" %>
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Instagram_API_Practice._Default" ClientIDMode="Static" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
 
     <div class="jumbotron">
         <h1>Instagram API Practice</h1>
         <p class="lead">Testing Instagram API...</p>
     </div>
 
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
+    <asp:Panel ID="authBegin" runat="server">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Login to Instagram</h2>
+                <asp:Button ID="btnLoginInstagram" runat="server" OnClick="btnLoginInstagram_Click" Text="Authenticate" />
+                <asp:Label ID="lblMessage" runat="server"></asp:Label>
+            </div>
         </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
+    </asp:Panel>
+
+
+    <asp:Panel ID="authSuccess" runat="server" Visible="false">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>API Response</h2>
+                <table>
+                    <tr>
+                        <td>Access Token</td>
+                        <td>
+                            <asp:Label ID="lblAccessToken" runat="server" Text="Unknown"></asp:Label></td>
+                    </tr>
+                    <tr>
+                        <td>User ID
+                        </td>
+                        <td>
+                            <asp:Label ID="lblUserID" runat="server" Text="Unknown"></asp:Label></td>
+                    </tr>
+                    <tr>
+                        <td>Full name</td>
+                        <td>
+                            <asp:Label ID="lblFullname" runat="server" Text="Unknown"></asp:Label></td>
+                    </tr>
+                    <tr>
+                        <td>Profile picture</td>
+                        <td>
+                            <asp:Image ID="imgProfilePicture" runat="server" /></td>
+                    </tr>
+                </table>
+            </div>
         </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
-    </div>
+    </asp:Panel>
 
 </asp:Content>
--- Instagram API Practice/Default.aspx.cs
+++ Instagram API Practice/Default.aspx.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +14,117 @@ namespace Instagram_API_Practice
 {
     public partial class _Default : Page
     {
+		// Global variable
+		public string myClientID = "2126fd843a7c4445b36098470692719d";
+		public string myClientSecret = "4920771ea8074e07bc5a08352d99194d";
+		public string instagramCode;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+			
+
+            // This part assumed that user already auth the app
+            if (!string.IsNullOrEmpty(Request.QueryString["code"]))
+            {
+				lblMessage.Text = "Authentication Successful";
+
+				// Save instagram code
+				instagramCode = Request.QueryString["code"];
+
+				// Post
+				exchangeCode();
+
+			}
+			else if (!string.IsNullOrEmpty(Request.QueryString["error"]))
+			{
+				lblMessage.Text = Request.QueryString["error"].ToString() + " because " + 
+					Request.QueryString["error_reason"].ToString() + ": " + 
+					Request.QueryString["error_description"].ToString();
+			}
+        }
+
+		private void exchangeCode()
+		{
+			// Parameter
+			string postData = "client_id=" + myClientID +
+			                  "&client_secret=" + myClientSecret +
+			                  "&grant_type=" + "authorization_code" +
+			                  "&redirect_uri=" + GetMyAddress() +
+			                  "&code=" + instagramCode;
+			byte[] byteArray = Encoding.ASCII.GetBytes(postData);
+
+			WebRequest request = WebRequest.Create("https://api.instagram.com/oauth/access_token");
+			request.Method = "POST";
+
+			request.ContentLength = postData.Length;
+			request.ContentType = "application/x-www-form-urlencoded";
+
+			Stream stream = request.GetRequestStream();
+			stream.Write(byteArray, 0, byteArray.Length);
+			stream.Close();
+
+			// Actual POSTing
+			HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+
+			// Read data
+			stream = response.GetResponseStream();
+			// Open the stream using a StreamReader for easy access.
+			StreamReader reader = new StreamReader(stream);
+
+			// Store data
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				lblMessage.Text = "Succes exchange data";
+
+				// Read the content
+				string responseFromInstagram = reader.ReadToEnd();
+				dynamic instagramAccessToken = JsonConvert.DeserializeObject(responseFromInstagram);
+
+				// Example of Structure of JSON
+				//{
+				//	"access_token": "fb2e77d.47a0479900504cb3ab4a1f626d174d2d",
+				//	"user": {
+				//		"id": "1574083",
+				//		"username": "snoopdogg",
+				//		"full_name": "Snoop Dogg",
+				//		"profile_picture": "..."
+				//	}
+				//}
+
+				lblAccessToken.Text = instagramAccessToken["access_token"];
+				lblUserID.Text = instagramAccessToken["user"]["id"];
+				lblFullname.Text = instagramAccessToken["user"]["full_name"];
+				imgProfilePicture.ImageUrl = instagramAccessToken["user"]["profile_picture"];
+
+				// hide panel
+				authBegin.Visible = false;
+				authSuccess.Visible = true;
+			}
+			else
+			{
+				authBegin.Visible = true;
+				authSuccess.Visible = false;
+			}
+
+			// Clean up the streams.
+			reader.Close();
+			response.Close();
+		}
+
+        protected void btnLoginInstagram_Click(object sender, EventArgs e)
+        {
+            // Return current localhost + its port
+			string myURI = GetMyAddress(); 
+            string redirectInstagram = "https://api.instagram.com/oauth/authorize/?client_id=" + myClientID + "&redirect_uri=" + myURI + "&response_type=code";
+
+            // Ask the user to auth this apps
+            Response.Redirect(redirectInstagram);
 
         }
+
+		private string GetMyAddress()
+		{
+			return Request.Url.GetLeftPart(UriPartial.Authority) + Request.Path;
+		}
     }
 }
